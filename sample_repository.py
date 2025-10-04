dc5ee91cd9d38f2d227cba9ee52e56a66a41e761
Ali Zabihian <zabihianali95@gmail.com>
2025-10-04 19:33:49 +0330

    sample repo added

---
+++ sample_repository.py
@@ -0,0 +1,11 @@
+class SampleRepository:
+    def save(self, sample):
+        pass
+    def update(self, sample):
+        pass
+    def delete(self, id):
+        pass
+    def find_all(self):
+        pass
+    def find_by_id(self, id):
+        pass
